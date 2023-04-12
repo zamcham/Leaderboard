60439e962edc41a5526fa491017b6b2074542302
zamcham <cristian@zamcham.com>
2023-04-12 14:05:35 -0400

    Add api request handle module

---
+++ src/api.js
@@ -0,0 +1,28 @@
+const BASE_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/UZPH3aMXFyBkcHNEozdZ';
+
+// Get all scores for a game
+export const getAllScores = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/scores/`);
+    const data = await response.json();
+    return data.result;
+  } catch (error) {
+    throw new Error('Failed to get scores');
+  }
+};
+
+// Submit a score for a game
+export const submitScore = async (fullName, score) => {
+  try {
+    const response = await fetch(`${BASE_URL}/scores/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user: fullName, score: score }),
+    });
+    const data = await response.json();
+  } catch (error) {
+    throw new Error('Failed to submit score');
+  }
+};
